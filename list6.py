c667d532b7c090aa02dd08fcf5d998f6639234ec
rupaliwaghmare <82706143+rupaliwaghmare@users.noreply.github.com>
2021-10-02 14:06:39 +0530

    Add files via upload

---
+++ list6.py
@@ -0,0 +1,52 @@
+# a=input("enter the string")
+# b=a[-1::-1]
+# if (a==b):
+#     print("palindrom")
+# else:
+#     print("not palindrom")
+
+# a=["n","i","t","i","n"]
+# a=input("enter the name")
+# b=[]
+# length=len(a)
+# print(length)
+# i=1
+# while i<=len(a):
+#     b.append(a[-i])
+#     i=i+1
+# print(b)  
+# if b==a:
+#     print("It is palindrome")  
+# else:
+#     print("It is not palindrom")
+
+# num=int(input("enter the number"))
+# i=0
+# a=num
+# while num>0:
+#     i=(i*10)+num%10
+#     num=num//10
+# if a==i:
+#     print("it is a palindrom")
+# else:
+#     print("it is not palindrom")
+
+# n=["n","i","t","i","n"]
+# i=0
+# b=[]
+# while i<len(n):
+#     b.append(n[i])
+#     i=i+1
+# print(b)
+# if n==b:
+#     print("palindrome hai")
+# else:
+#     print("palindrome nahi hai")
+
+n=(input("enter the number"))
+a=[]
+a.append(n)
+print(a)
+
+
+
