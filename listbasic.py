73ed0efe3b9176ce70a609222a4c7f9e03fdd1b8
rupaliwaghmare <82706143+rupaliwaghmare@users.noreply.github.com>
2021-10-02 13:13:05 +0530

    Add files via upload

---
+++ listbasic.py
@@ -0,0 +1,18 @@
+# list=[]
+# i=0
+# while i<10:
+#     list.append(i)
+#     i=i+1
+# print(list)
+
+
+list=[1,2,3,4,5,6,7,8]
+n=int(input("enter the number"))
+a=[]
+i=0
+while i<len(list):
+    if i<len(list):
+        print("list mai hai")
+    else:
+        print("list mai nahi hai")
+    i=i+1
