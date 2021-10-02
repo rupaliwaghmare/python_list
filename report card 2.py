eaceda5089996b1e6c646ad0611a3aa964ffa905
rupaliwaghmare <82706143+rupaliwaghmare@users.noreply.github.com>
2021-10-02 13:10:33 +0530

    Add files via upload

---
+++ report card 2.py
@@ -0,0 +1,79 @@
+# 1
+
+marks = [
+
+    [78, 76, 94, 86, 88],
+
+    [91, 71, 98, 65, 76],
+
+    [95, 45, 78, 52, 49]
+
+]
+
+i=0
+count=0
+while i<len(marks):
+    j=0
+    sum=0
+    a=0
+    while j<len(marks[i]):
+        a=a+marks[i][j]
+        j=j+1
+    sum=sum+a
+    average=sum/len(marks[i])
+    i=i+1
+    count=count+1
+    print("average",average)
+
+#  2
+
+# marks = [
+
+#     [78, 76, 94, 86, 88],
+
+#     [91, 71, 98, 65],
+
+#     [95, 45, 78]
+
+# ]
+# i=0
+# count=0
+# while i<len(marks):
+#     j=0
+#     sum=0
+#     a=0
+#     while j<len(marks[i]):
+#         a=a+marks[i][j]
+#         j=j+1
+#     sum=sum+a
+#     average=sum/len(marks[i])
+#     i=i+1
+#     count=count+1
+#     print("average",average)
+
+# 3  
+# marks = [
+
+#     [78, 76, 94, 86, 88],
+
+#     [91, 71, 98, 65],
+
+#     [95, 45, 78],
+
+#     [87, 67, 49, 68, 88]
+
+# ]
+# i=0
+# count=0
+# while i<len(marks):
+#     j=0
+#     sum=0
+#     a=0
+#     while j<len(marks[i]):
+#         a=a+marks[i][j]
+#         j=j+1
+#     sum=sum+a
+#     average=sum/len(marks[i])
+#     i=i+1
+#     count=count+1
+#     print("average",average)
