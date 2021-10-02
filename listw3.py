aa7d3f862e6259484f11c3370b96d8e9ddb482f3
rupaliwaghmare <82706143+rupaliwaghmare@users.noreply.github.com>
2021-10-02 13:12:16 +0530

    Add files via upload

---
+++ listw3.py
@@ -0,0 +1,38 @@
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[1])
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits[0]="kiwi"
+# print(fruits)
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+# i="orange"
+# fruits.append(i)
+# print(fruits)
+
+
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.insert(1,"lemon")
+# print(fruits)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.remove("banana")
+# print(fruits)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[-1])
+
+# fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(fruits[2:5])
+
+# fruits = ["apple", "banana", "cherry"]
+# print(len(fruits))
+
+
