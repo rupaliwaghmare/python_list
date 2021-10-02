997b326bb0382a3ce5bb28b9743b4cdd2f65eff9
rupaliwaghmare <82706143+rupaliwaghmare@users.noreply.github.com>
2021-10-02 13:12:00 +0530

    Add files via upload

---
+++ logicque.py
@@ -0,0 +1,25 @@
+name=["sharadda","rupali","anita"]
+i=0
+while i<len(name):
+    a=len(name[i])
+    print(name[i],a)
+    i=i+1
+
+   
+
+
+# name=["Sakshi","Rupali","Anita"]
+# i=0
+# while i<len(name):
+#     a=len(name[i])
+#     if a%2==0:
+#         print(name[i],a,"even number")
+#     else:
+#         print(name[i],a,"odd number")
+#     i=i+1
+
+
+
+
+
+
