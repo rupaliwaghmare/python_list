faaf4cc7788090666e9d130dd7dfec61de4e2c4f
rupaliwaghmare <82706143+rupaliwaghmare@users.noreply.github.com>
2021-10-02 10:19:44 +0530

    Add files via upload

---
+++ total sum.py
@@ -0,0 +1,22 @@
+# number = 30
+
+# n = [10, 11, 12, 13, 14, 17, 18, 19]
+# i=0
+# a=[]
+# k=0
+# while i<len(n)/2:
+#     b=1
+#     while b<len(n):
+#         if n[i]+n[b]==number:
+#             k=([n[i],n[b]])
+#             a.append(k)
+#         b=b+1
+#     i=i+1
+# print(a)
+
+
+a="razia"
+if a in" z":
+    print("z hai")
+
+
