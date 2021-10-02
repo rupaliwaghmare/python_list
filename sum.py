eea55e87b5e692246c94f0ee888761edbda900a0
rupaliwaghmare <82706143+rupaliwaghmare@users.noreply.github.com>
2021-10-02 10:19:59 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,19 @@
+list1=[1,2,3,4,5]
+# list2=[1,2,3,4,5]
+# list3=[]
+# i=0
+# while i<len(list1):
+#     list3.append(list1[i]+list2[i])
+#     i=i+1
+# print("sum of two list:",list3)
+
+
+list1=["m","na","i","rup"]
+list2=["y","me","s","ali"]
+list3=[]
+i=0
+while i<len(list1):
+    list3.append (str(list1[i])+ str(list2[i]))
+    i=i+1
+print (str(list3))
+
